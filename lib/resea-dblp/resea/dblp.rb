79be61b0ccff51c79ce9d63eca694ae8a89e8bc5
Bernard Lambeau <blambeau@gmail.com>
2012-02-08 17:26:50 +0100

    Add DBLP::Grab

--- lib/resea-dblp/resea/dblp.rb
+++ lib/resea-dblp/resea/dblp.rb
@@ -1,7 +1,46 @@
 module Resea
   module DBLP
+
+    def grab(identifier)
+      Grab.new.call(identifier)
+    end
+    module_function :grab
+
+    private
+
+    include Resea::ErrorUtils
+
+    def url
+      "http://www.dblp.org"
+    end
+
+    def entry_url(identifier)
+      "#{url}/rec/bibtex/#{identifier}.xml"
+    end
+
+    def search_url
+      "#{url}/search/api"
+    end
+
+    def get(url)
+      got = Http.get(url, :response => :object)
+      case got.status
+      when 200...300
+        not_found_error!(url) if got.body.empty?
+        got.body
+      when 400...500
+        not_found_error!(url)
+      when 500...600
+        third_party_error!(url)
+      else
+        unexpected_error!(url)
+      end
+    end
+
   end # module DBLP
 end # module Resea
 require_relative 'dblp/parser'
 require_relative 'dblp/entry_parser'
 require_relative 'dblp/query_result_parser'
+require_relative 'dblp/grab'
+
---
+++ lib/resea-dblp/resea/dblp/grab.rb
@@ -0,0 +1,14 @@
+module Resea
+  module DBLP
+    class Grab
+      include DBLP
+
+      def call(identifier)
+        xml    = get entry_url(identifier)
+        tuples = EntryParser.parse(xml)
+        tuples.first
+      end
+
+    end # class Grab
+  end # module DBLP
+end # module Resea
